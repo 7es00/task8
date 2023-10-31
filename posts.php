8af7d7d5cfe714194e2ed32005c113a1afec02b7
Mohammed Mostafa <88839991+7es00@users.noreply.github.com>
2023-11-01 01:17:38 +0200

    Delete posts.php

--- posts.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="style.css">
-
-<?php
-require('./connection.php');
-$sql = "select * from `posts`";
-$query = $con->prepare($sql);
-$query->execute();
-$collection = $query->fetchAll(PDO::FETCH_ASSOC);
-require('./data_view.php');
